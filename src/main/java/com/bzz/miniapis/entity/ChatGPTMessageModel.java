57673aa0773510b81d95ace409272ed476f9bc98
BaoZZ <ddnzero@126.com>
2023-03-10 17:12:25 +0800

    增加gpt实体类

---
+++ src/main/java/com/bzz/miniapis/entity/ChatGPTMessageModel.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2023 bzz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bzz.miniapis.entity;
+
+import java.io.Serializable;
+
+/**
+ * 根据文档封装的chatGPT的消息实体
+ *
+ * @author bzz
+ * @see https://platform.openai.com/docs/api-reference/chat
+ */
+public class ChatGPTMessageModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String role;    //角色
+    private String content; //对话内容
+
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
